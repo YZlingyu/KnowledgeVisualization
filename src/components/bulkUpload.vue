1c9fee0807708013bae6653eefee5a3616da645c
YZ <741529838@qq.com>
2017-12-19 20:03:34 +0800

    1

--- src/components/bulkUpload.vue
+++ src/components/bulkUpload.vue
@@ -7,13 +7,15 @@
    <el-upload
       class="upload-demo"
       drag
-      action=""
+      action="https://jsonplaceholder.typicode.com/posts/"
       :on-preview="handlePreview"
+      :on-success="handleAvatarSuccess"
       :on-remove="handleRemove"
       multiple
       :limit="3"
       :on-exceed="handleExceed"
-      :file-list="fileList">
+      :file-list="fileList"
+      :before-upload="beforeAvatarUpload">
       <i class="el-icon-upload"></i>
       <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
       <div class="el-upload__tip" slot="tip"></div>
@@ -37,9 +39,37 @@
       handlePreview(file) {
         console.log(file);
       },
+      handleAvatarSuccess(res, file,fileList) {
+       /* this.imageUrl = URL.createObjectURL(file.raw);
+        console.log(this.imageUrl);
+        let content = document.getElementsByClassName("upload-demo")[0].childNodes[0].childNodes[1].files;
+        console.log(content);*/
+        console.log(file,fileList)
+        var reader = new FileReader();
+        reader.onload = function()
+        {
+          console.log(this) ;
+        };
+        reader.readAsText(file);
+      },
       handleExceed(files, fileList) {
         this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
+      },
+      beforeAvatarUpload(file) {
+        const isCSV = file.type === 'text/csv';
+        const isLt2M = file.size / 1024 / 1024 < 2;
+
+        if (!isCSV) {
+          this.$message.error('上传头像图片只能是 csv 格式!');
+        }
+        if (!isLt2M) {
+          this.$message.error('上传头像图片大小不能超过 2MB!');
+        }
+        return isCSV && isLt2M;
       }
+    },
+    mounted(){
+
     }
   }
 
